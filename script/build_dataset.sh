fbea9fbe01406d9b0a7fc53293644176da87a53d
hyun <shpark26@postech.ac.kr>
2024-08-27 07:16:18 +0000

    upd - dataset generation

---
+++ script/build_dataset.sh
@@ -0,0 +1,11 @@
+cd ../util
+
+# State list: alpha_P, c5, c7ax
+TEMP=( 100.0 200.0 300.0 400.0 500.0 600.0 )
+for (( i=0; i<${#TEMP[@]}; i++ ));
+do
+    CUDA_VISIBLE_DEVICES=$(($i + 2)) python build_dataset.py \
+        --molecule alanine \
+        --state $1 \
+        --temperature ${TEMP[i]} &
+done
--- util/build_dataset.py
+++ util/build_dataset.py
@@ -24,12 +24,12 @@ parser = argparse.ArgumentParser(description="Dataset builder")
 # Simluation arguments
 parser.add_argument("--molecule", type=str, help="Path to the PDB file", default="alanine")
 parser.add_argument("--state", type=str, help="Molecule state to start the simulation", default="c5")
-parser.add_argument("--temperature", type=float, help="Temperature to use", default=300.0)
+parser.add_argument("--temperature", type=float, help="Temperature to use", default=273.0)
 args = parser.parse_args()
 
 
 class MD_Dataset(Dataset):
-    def __init__(self, traj, config):
+    def __init__(self, loaded_traj, config):
         self.molecule = config['molecule']
         self.state = config['state']
         self.temperature = config['temperature']
@@ -38,21 +38,23 @@ class MD_Dataset(Dataset):
         self.solvent = config['solvent']
         self.platform = config['platform']
         self.precision = config['precision']
+        self.device = "cuda"
         
         data_x_list = []
         data_y_list = []
+        traj = loaded_traj.xyz.squeeze()
         for t in tqdm(
-            range(self.time -1),
+            range((self.time -1) // 10),
             desc="Loading data"
         ):
-            current_state = torch.tensor(loaded_traj[t].xyz.squeeze())
-            next_state = torch.tensor(loaded_traj[t+1].xyz.squeeze())
+            current_state = torch.tensor(loaded_traj[t].xyz.squeeze()).to(self.device)
+            next_state = torch.tensor(loaded_traj[t+1].xyz.squeeze()).to(self.device)
             data_x_list.append(current_state)
             data_y_list.append(next_state)
-        self.x = torch.stack(data_x_list)
-        self.y = torch.stack(data_y_list)
+        self.x = torch.stack(data_x_list).to(self.device)
+        self.y = torch.stack(data_y_list).to(self.device)
         
-        self.sanity_check(loaded_traj)
+        # self.sanity_check(loaded_traj)
     
     def sanity_check(self, loaded_traj):
         # print("Running sanity check...")
@@ -66,8 +68,8 @@ class MD_Dataset(Dataset):
         ):
             x = self.x[t]
             y = self.y[t]
-            x_frame = torch.tensor(loaded_traj[t].xyz.squeeze())
-            y_frame = torch.tensor(loaded_traj[t+1].xyz.squeeze())
+            x_frame = torch.tensor(loaded_traj[t].xyz.squeeze()).to(self.device)
+            y_frame = torch.tensor(loaded_traj[t+1].xyz.squeeze()).to(self.device)
             
             assert torch.equal(x, x_frame), f"Frame {t}, x not equal"
             assert torch.equal(y, y_frame), f"Frame {t+1}, y not equal"        
@@ -103,12 +105,13 @@ if __name__ == "__main__":
     # print(f"Done.!! ({end - start:.2f} sec)\n")
     
     # Build dataset
-    print(f"Building dataset for {args.temperature}K, sttate {args.molecule} ...")
+    print(f"Building dataset for {args.temperature}K, state {args.state} ...")
     dataset = MD_Dataset(loaded_traj, config)
     save_dir = f"../dataset/{args.molecule}/{args.temperature}"
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
+    if os.path.exists(f"{save_dir}/{args.state}.pt"):
+        raise ValueError(f"Folder {save_dir}/{args.state}.pt already exists")
     else:
-        raise ValueError(f"Directory {save_dir} already exists")
-    torch.save(dataset, f"{save_dir}/{args.state}.pt")
+        torch.save(dataset, f"{save_dir}/{args.state}.pt")
     print("Done.!!")
