1a14eb306f2ed76280b4eb7ff2a46c40543f462f
hyun <shpark26@postech.ac.kr>
2025-01-17 02:07:08 +0000

    upd - 10ns dataset construction

--- script/build-dataset-cl-alanine.sh
+++ script/build-dataset-cl-alanine.sh
@@ -3,9 +3,9 @@ cd ../
 python build_cl_dataset_v2.py \
     --molecule alanine \
     --temperature 300.0 \
-    --dataset_size 100000 \
-    --dataset_version v8 \
-    --positive_sample_augmentation 10 \
-    --negative_sample_augmentation 100 \
-    --traj_dir 24-12-26/14:53 \
-    --traj_dir 24-12-26/15:08 
+    --dataset_size 1000000 \
+    --dataset_version 10n-v2 \
+    --positive_sample_augmentation 100 \
+    --negative_sample_augmentation 10000 \
+    --traj_dir 24-12-26/15:24 \
+    --traj_dir 24-12-26/17:52 
